0ac1390c7d288c4c77fbee26d09ebe94a44a051b
Kyle Okura <kyle.okura@yahoo.com>
2024-04-10 20:42:38 -0600

    updating server.spec with correct route

--- ProjectSourceCode/test/server.spec.js
+++ ProjectSourceCode/test/server.spec.js
@@ -39,25 +39,20 @@ describe('Server!', () => {
 // Explanation: The testcase will call the /add_user API with the following input
 // and expects the API to return a status of 200 along with the "Success" message.
 
+
+//We are checking POST /add_user API by passing the user info in in incorrect manner (name cannot be an integer). This test case should pass and return a status 400 along with a "Invalid input" message.
+
 describe('Testing Add User API', () => {
-  it('positive : /add_user', done => {
+  it('positive : /register', done => {
     chai
       .request(server)
-      .post('/add_user')
+      .post('/register')
       .send({username: 'Test User', password: 'TestUserPassword'})
       .end((err, res) => {
         expect(res).to.have.status(200);
         expect(res.body.message).to.equals('Success');
         done();
       });
-  });
-});
-
-
-//We are checking POST /add_user API by passing the user info in in incorrect manner (name cannot be an integer). This test case should pass and return a status 400 along with a "Invalid input" message.
-
-describe('Testing Add User API', () => {
-  it('positive : /add_user', done => {
     // Refer above for the positive testcase implementation
   });
 
@@ -68,10 +63,10 @@ describe('Testing Add User API', () => {
   // Result: This test case should pass and return a status 400 along with a "Invalid input" message.
   // Explanation: The testcase will call the /add_user API with the following invalid inputs
   // and expects the API to return a status of 400 along with the "Invalid input" message.
-  it('Negative : /add_user. Checking invalid name', done => {
+  it('Negative : /register. Checking invalid name', done => {
     chai
       .request(server)
-      .post('/add_user')
+      .post('/register')
       .send({username: 10, password: 'userpass'})
       .end((err, res) => {
         expect(res).to.have.status(400);
